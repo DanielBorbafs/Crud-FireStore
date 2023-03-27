3f4d8ccb7df3ed6946ef8027c1b76650ae26d32c
Danielborbafs <dev.danielborba@gmail.com>
2023-03-27 04:52:03 -0300

    buttons

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,10 +1,32 @@
-export default function Home() {
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
+export default function Home() {
+  const clientes =[
+    new Cliente('Daniel', 21, '1'),
+    new Cliente('Sofia', 21, '2'),
+    new Cliente('Cristian', 21, '3'),
+  ]
+  function clienteSelecionado(cliente:Cliente) {
+    console.log(cliente.nome)
+
+  }
+  function clienteExcluido(cliente:Cliente) {
+    console.log(cliente.nome)
+  }
   return (
     <div className={`
-      flex h-screen justify-center items-center
+      flex h-screen justify-center items-center 
+      bg-gradient-to-r from-blue-700 to-purple-800
+      text-white
    `}>
-      <span className="text-4xl">Texto</span>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}
+         clienteSelecionado={clienteSelecionado}
+         clienteExcluido={clienteExcluido}
+        />
+      </Layout>
     </div>
   )
 }
