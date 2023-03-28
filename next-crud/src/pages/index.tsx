fcbb739849992eea4b58448aed8a8a996411f41d
Danielborbafs <dev.danielborba@gmail.com>
2023-03-28 14:54:53 -0300

    botao add cliente

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
+import Botao from "../components/botao"
 
 export default function Home() {
   const clientes =[
@@ -22,6 +23,9 @@ export default function Home() {
       text-white
    `}>
       <Layout titulo="Cadastro Simples">
+        < div className="flex justify-end">
+          <Botao cor="blue" className="mb-4">Novo Cliente</Botao>
+        </div>
         <Tabela clientes={clientes}
          clienteSelecionado={clienteSelecionado}
          clienteExcluido={clienteExcluido}
@@ -30,3 +34,4 @@ export default function Home() {
     </div>
   )
 }
+
