6ac2933b25a66cdfc2ae254c0f29a46f2cc1ca5e
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-04-03 06:11:58 -0300

    Update index.tsx

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,40 +1,51 @@
+import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
-import Tabela from "../components/Tabela"
-import Cliente from "../core/Cliente"
-import Botao from "../components/Botao"
-import Formulario from "./components/Entrada";
+import Tabela from "../components/Tabela";
+
+import useClientes from "../hooks/useClientes";
 
 export default function Home() {
-  const clientes =[
-    new Cliente('Daniel', 21, '1'),
-    new Cliente('Sofia', 21, '2'),
-    new Cliente('Cristian', 21, '3'),
-  ]
-  function clienteSelecionado(cliente:Cliente) {
-    console.log(cliente.nome)
-    const [visivel, setVisivel] = useState <'tabela' | 'form'> ('tabela')
 
-  }
-  function clienteExcluido(cliente:Cliente) {
-    console.log(cliente.nome)
-  }
+  const {
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
+
   return (
     <div className={`
-      flex h-screen justify-center items-center 
-      bg-gradient-to-r from-blue-700 to-purple-800
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-   `}>
+    `}>
       <Layout titulo="Cadastro Simples">
-        < div className="flex justify-end">
-          <Botao cor="blue" className="mb-4">Novo Cliente</Botao>
-        </div>
-        <Tabela clientes={clientes}
-         clienteSelecionado={clienteSelecionado}
-         clienteExcluido={clienteExcluido}
-        />
-        <Formulario cliente={clientes[0]}></Formulario>
+        {tabelaVisivel ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="green" className="mb-4"
+                onClick={novoCliente}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela clientes={clientes}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
+            />
+          </>
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
+          />
+        )}
       </Layout>
     </div>
   )
 }
-
